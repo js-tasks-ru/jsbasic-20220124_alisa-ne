38a8cc0bb8b05c3f71f966640f84c8c63b2f3213
Alisa-InWonderland <alisa_ne@yahoo.com>
2022-01-27 15:54:13 +0100

    1st task

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,14 @@
 function factorial(n) {
-  // ваш код...
+
+	if (n === 0 || n === 1) {
+		return 1;
+	}
+	
+	let result = n;
+	
+	for (let i = n - 1; i >= 1; i--) { 
+		result = result * i;
+		
+	}
+	return result;
 }
