7bd0893209deb275dee018bc25fbb493861aa583
Alisa-InWonderland <alisa_ne@yahoo.com>
2022-02-15 19:13:31 +0100

    4th module is completed

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,37 @@
 function highlight(table) {
-  // ваш код...
+	
+	let rows = table.rows;
+
+	for (let i = 1; i < rows.length; i++) {
+		
+		let targetRow = rows[i];
+
+		let ageCell = rows[i].cells[1];
+
+		if (ageCell.textContent < 18) {
+			targetRow.setAttribute('style', 'text-decoration: line-through');
+		}
+
+		let genderCell = rows[i].cells[2];
+		
+		if (genderCell.textContent === 'm') {
+			targetRow.classList.add('male');
+
+		} else if (genderCell.textContent === 'f') {
+			targetRow.classList.add('female');
+		}
+		
+		let statusCell = rows[i].cells[3];
+		
+		if (statusCell.getAttribute('data-available') === 'true') {
+			targetRow.classList.add('available');
+
+		} else if (statusCell.getAttribute('data-available') === 'false') {
+			targetRow.classList.add('unavailable');
+
+		} else if (!statusCell.hasAttribute('data-available')) {
+			targetRow.setAttribute('hidden', 'true');
+		}
+
+	}
 }
