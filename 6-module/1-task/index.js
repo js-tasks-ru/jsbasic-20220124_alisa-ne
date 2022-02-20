7e69a18ddfce63620fe43e385fff0541009d526e
Alisa-InWonderland <alisa_ne@yahoo.com>
2022-02-20 20:08:37 +0100

    1st task is completed

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable indent */
 /**
  * Компонент, который реализует таблицу
  * с возможностью удаления строк
@@ -13,6 +14,54 @@
  *
  */
 export default class UserTable {
-  constructor(rows) {
-  }
-}
+	constructor(rows) {
+		this.rows = rows;
+		this.elem = this.render();
+	}
+
+	render() {
+		this.table = document.createElement('TABLE');
+		this.table.insertAdjacentHTML('afterBegin', 
+			`<thead>
+				<tr>
+					<th>Имя</th>
+					<th>Возраст</th>
+					<th>Зарплата</th>
+					<th>Город</th>
+					<th></th>
+				</tr>
+    		</thead>
+			<tbody>
+			</tbody>`);
+		
+		this.tbody = this.table.querySelector('tbody');
+
+		for (const row of this.rows) {
+			this.tbody.insertAdjacentHTML('beforeEnd', 
+			`<tr>
+            <td>${row.name}</td>
+            <td>${row.age}</td>
+            <td>${row.salary}</td>
+            <td>${row.city}</td>
+            <td><button class='btn'>X</button></td>
+        </tr>`);
+		}
+		
+		this.buttons = this.table.querySelectorAll('.btn');
+
+		for (const btn of this.buttons) {
+		btn.addEventListener('click', this.onClick);
+		}
+
+		return this.table;
+
+	}
+
+	onClick(event) {
+		const row = event.target.closest('tr');
+
+		if (row) {
+			row.remove();
+			}
+		}
+	}
