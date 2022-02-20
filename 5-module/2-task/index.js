deb2abbe1b9159461810ab6915040cc5e2419da0
Alisa-InWonderland <alisa_ne@yahoo.com>
2022-02-20 10:54:37 +0100

    5th module is completed

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,15 @@
+/* eslint-disable indent */
 function toggleText() {
-  // ваш код...
+	const btn = document.querySelector(`.toggle-text-button`);
+	const text = document.querySelector(`#text`);
+	let count = 0;
+
+	function hideShow() { 
+		count++;
+		count % 2 === 0 ?
+			text.hidden = false
+			: text.hidden = true;
+	}
+
+	btn.addEventListener("click", hideShow);
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,37 @@
+/* eslint-disable indent */
 function initCarousel() {
-  // ваш код...
+
+	const btnRight = document.querySelector('.carousel__arrow_right');
+	const btnLeft = document.querySelector('.carousel__arrow_left');
+	const carouselInner = document.querySelector('.carousel__inner');
+	const carouselInnerWidth = carouselInner.offsetWidth;
+
+	let translateX = 0;
+	btnLeft.style.display = 'none';
+
+	function moveRight() {
+		
+		translateX -= carouselInnerWidth;
+		carouselInner.style.transform = `translateX(${translateX}px)`;
+
+		if (translateX < 0) {
+			btnLeft.style.display = '';
+		}
+		if (translateX === -carouselInnerWidth * 3) {
+			btnRight.style.display = 'none';
+		}	
+	} 
+
+	function moveLeft() {
+	
+		translateX += carouselInnerWidth;
+		carouselInner.style.transform = `translateX(${translateX}px)`;
+
+		if (translateX === carouselInnerWidth * 3 || translateX === 0) {
+			btnLeft.style.display = 'none';
+		}
+	}
+
+	btnRight.addEventListener("click", moveRight);
+	btnLeft.addEventListener("click", moveLeft);
 }
